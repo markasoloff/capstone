a8b03351b2a70a2195648306d1142ae0e1d0acd2
“Marksoloff” <marksoloff@gmail.com>
2018-10-28 13:33:23 -0500

    fixed index render

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -12,12 +12,13 @@ class Api::ArticlesController < ApplicationController
 
 
   def index
-    response = HTTP.get("https://api.nytimes.com/svc/search/v2/articlesearch.json")
-    render json: response.parse
+    @articles = Article.all 
+    # response = HTTP.get("https://api.nytimes.com/svc/search/v2/articlesearch.json")
+    # render json: response.parse
 
     # @articles = Article.all
     # # @articles = @articles.where(remote: true)
-    # render "index.json.jbuilder"
+    render "index.json.jbuilder"
   end
 
   def show
