c98705d8c8a6ec28829fdc6350b174e2e63d9528
“Marksoloff” <marksoloff@gmail.com>
2018-10-29 21:34:53 -0500

    nyt index added

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,25 +1,17 @@
 class Api::ArticlesController < ApplicationController
   # before_action :authenticate_admin, except: [:index, :show]
 
+  def index
+    response = HTTP.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=trump&api-key=#{ENV['API_KEY']}")
+    data = response.parse
+    render json: data
+  end
+
   # def index
   #   @articles = Article.all
-  #   respond_to do |format|
-  #     format.html
-  #     format.json { render :json => @articles }
-  #   end
+  #   render "index.json.jbuilder"
+  #   # render json: @articles.parse
   # end
-
-
-
-  def index
-    @articles = Article.all 
-    # response = HTTP.get("https://api.nytimes.com/svc/search/v2/articlesearch.json")
-    # render json: response.parse
-
-    # @articles = Article.all
-    # # @articles = @articles.where(remote: true)
-    render "index.json.jbuilder"
-  end
 
   def show
     @article = Article.find(params[:id])
---
+++ app/controllers/api/spare_code.rb
@@ -0,0 +1,10 @@
+# API KEY ATTEMPTS
+
+
+  # def index
+  #   response = HTTP.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=#{ ENV['NEWS_API_KEY'] }")
+  #   render json: response.parse
+  # end
+
+  # response = HTTP.headers("X-AYLIEN-TextAPI-Application-Key" => "c5ed8135a916dc84d9b459e8dbd20841", "X-AYLIEN-TextAPI-Application-ID" => "db116eaf").get("https://api.aylien.com/api/v1/concepts?url=#{params[:url]}")
+#     render json: response.parse
