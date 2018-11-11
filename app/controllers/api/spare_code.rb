6d658956d9baf2917ec5086158619b9ffe22a56f
“Marksoloff” <marksoloff@gmail.com>
2018-11-11 17:09:42 -0600

    classes added to sub

--- app/controllers/api/spare_code.rb
+++ app/controllers/api/spare_code.rb
@@ -1,40 +1,55 @@
-# API KEY ATTEMPTS
+# # API KEY ATTEMPTS
 
 
-  # def index
-  #   response = HTTP.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=#{ ENV['NEWS_API_KEY'] }")
-  #   render json: response.parse
-  # end
+#   # def index
+#   #   response = HTTP.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=#{ ENV['NEWS_API_KEY'] }")
+#   #   render json: response.parse
+#   # end
 
-  # response = HTTP.headers("X-AYLIEN-TextAPI-Application-Key" => "c5ed8135a916dc84d9b459e8dbd20841", "X-AYLIEN-TextAPI-Application-ID" => "db116eaf").get("https://api.aylien.com/api/v1/concepts?url=#{params[:url]}")
-#     render json: response.parse
-
-
-pattern = " is"
-
-p "this is the way...".gsub(/(?<= pattern)/, " not")
+#   # response = HTTP.headers("X-AYLIEN-TextAPI-Application-Key" => "c5ed8135a916dc84d9b459e8dbd20841", "X-AYLIEN-TextAPI-Application-ID" => "db116eaf").get("https://api.aylien.com/api/v1/concepts?url=#{params[:url]}")
+# #     render json: response.parse
 
 
+# # pattern = " is"
 
-, "Sessions" => "meatboy", "US" => "The United Snapes of America", "economic" => "money-shenanigans", "country" => "citizen festival site", " global" => "transdimensional", "commission" => "fun-time boy party", "Washington" => "The fertile womb of government", " tax" => "tithings, m'lord", "business" => "cash magic", "refused" => "scoffed, loud and hard about ", " region" => "beastlands", "Europe" => "That Nicey-nicey continent", "British" => "proto-American (aka British, son)", "minister" => "optimus", "spending" => "money torturing", "Britain" => "America's Grandpappy, Brittania", "senator" => "bug-lord", "Senate" => "Swarm of the bug-lords", "congress" => "bongress", "congressional" => "bongressional", "Pence" => "Reed Richards (aka Mr. Fantastic)", "argues" => "eagle-screeches from the roosts of the city", "government" => "fun police"
-
-"women" => "non-men, the fairer sex i'faith,", "story" => "a tale that would set your ass on fire", "recently" => "schmecently", "public" => "errybody's", "pollution" => "mung of the earth", "American" => "Boopmerican", "american", "schmoopmerican"     
-
-
-body.gsub(/Trump|Mr. Trump|president|President|presidency|Sessions|US|economic|country|global|commission|Washington|tax|business|refused|region|Europe|British|minister|spending|Britain|senator|Senate|congress|congressional|Pence|argues|government|
-
-
-  |women|story|recently|public|pollution|American|american
+# # p "this is the way...".gsub(/(?<= pattern)/, " not")
 
 
 
+# # , "Sessions" => "meatboy", "US" => "The United Snapes of America", "economic" => "money-shenanigans", "country" => "citizen festival site", " global" => "transdimensional", "commission" => "fun-time boy party", "Washington" => "The fertile womb of government", " tax" => "tithings, m'lord", "business" => "cash magic", "refused" => "scoffed, loud and hard about ", " region" => "beastlands", "Europe" => "That Nicey-nicey continent", "British" => "proto-American (aka British, son)", "minister" => "optimus", "spending" => "money torturing", "Britain" => "America's Grandpappy, Brittania", "senator" => "bug-lord", "Senate" => "Swarm of the bug-lords", "congress" => "bongress", "congressional" => "bongressional", "Pence" => "Reed Richards (aka Mr. Fantastic)", "argues" => "eagle-screeches from the roosts of the city", "government" => "fun police"
+
+# # "women" => "non-men, the fairer sex i'faith,", "story" => "a tale that would set your ass on fire", "recently" => "schmecently", "public" => "errybody's", "pollution" => "mung of the earth", "American" => "Boopmerican", "american", "schmoopmerican"     
 
 
-  /)
+# # body.gsub(/Trump|Mr. Trump|president|President|presidency|Sessions|US|economic|country|global|commission|Washington|tax|business|refused|region|Europe|British|minister|spending|Britain|senator|Senate|congress|congressional|Pence|argues|government|
+
+
+# #   |women|story|recently|public|pollution|American|american
 
 
 
 
 
+# #   /)
 
-{ |match| matchers[match] }
+
+
+
+
+
+# # { |match| matchers[match] }
+
+
+
+# def add_body
+#   addition_hash = { "million" => "million dollars worth of bees, glorious bees",
+#                     "Tax" => "Tax baby, tax",
+#                     "tax" => "tax, or as ya boy Donny Trump says it: 'terx'",
+#                     "taxes" => "taxes, or as ya boy Donny Trump likes to call them: 'torxes'",
+#                     "elections" => "elections - more like ERECTIONS, am I right? Wait, sorry... elections." }
+#   matchers = addition_hash
+#   body.gsub(/elections|million|Tax|tax|taxes|    liberal|Liberal|democrat|Democrat|imposed|very|tariffs|China|conversation|argument|houses|democracy|quote/){ |match| matchers[match] }
+# end
+
+
+# , "liberal" => "liberal application of topical ointment to the hindquarters", "Liberal" => "Liberal, by which we mean a fowl of the eastern seaboard,", "democrat" => "democrat, the other white meat", "Democrat" => "Democrat, which is latin for 'a free version of the software' and 'crat',", "imposed" => "imposed with great vengeance and furious anger these ", "very" => "very minimal", "tariffs" => "tariffs, like from that one movie Star Wars: the Phantom Tarrifs", "China" => "China, land of sun and oats", "conversation" => "stimulating conversation over martinis", "argument" => "argument of electric swords as they clash and vie for the supremacy of their mighty weilders", "houses" => "haunted houses", "democracy" => "democracy, more like demoCRAZY, am I right friend? No? Ah, I guess you had to be there...    CONTINUED:", "quote" => "quothe" 
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,9 +4,40 @@ class Article < ApplicationRecord
 
 
   def swap_body
-    substitution_hash = { "Trump" => "\"Mothertruckin\" Trump",
-                "Mr. Trump" => "Donald \"Mothertruckin\" Trump",
-               "president" => "hyperdimensional samurai", "President" => "Hyperdimensional samurai", "presidency" => "glorious and honorable title of hyperdimensional samurai", "Sessions" => "\"Meatsy\" Sessions", "US" => "The United Snapes of America", "economic" => "money-shenanigans", "country" => "citizen festival site", " global" => "transdimensional", "commission" => "fun-time boy party", "Washington" => "The fertile womb of government", " tax" => "tithings, m'lord", "business" => "cash magic", "refused" => "scoffed, loud and hard about ", " region" => "beastlands", "Europe" => "That Nicey-nicey continent", "British" => "proto-American (aka British, son)", "minister" => "optimus", "spending" => "money torturing", "Britain" => "America's Grandpappy, Brittania", "senator" => "bug-lord", "Senate" => "Swarm of the bug-lords", "congress" => "bongress", "congressional" => "bongressional", "Pence" => "Reed Richards (aka Mr. Fantastic)", "argues" => "eagle-screeches from the roosts of the city", "government" => "fun police", "women" => "non-men, aye they be the fairer sex i'faith,", "story" => "a tale that would set your ass on fire", "recently" => "schmecently", "public" => "errybody's", "pollution" => "mung of the earth", "American" => "Boopmerican", "american" => "schmoopmerican", "Mueller" => "\"Platinum Johnson\" Mueller", "recused" => "\"brocused\"" }
+    substitution_hash = { "Trump" => "<span class='swap'>\"Mothertruckin\" Trump</span>",
+                "Mr. Trump" => "<span class='swap'>Mr. Trump</span>",
+               "president" => "<span class='swap'>hyperdimensional samurai</span>",
+               "President" => "<span class='swap'>Hyperdimensional samurai</span>",
+              "presidency" => "<span class='swap'>jellybean days</span>", "Sessions" => "<span class='swap'>meatboy</span>",
+               "US" => "<span class='swap'>The United Snapes of America</span>",
+                "economic" => "<span class='swap'>money-shenanigans</swap>",
+                 "country" => "<span class='swap'>citizen festival site</span>",
+                  " global" => "<span class='swap'>transdimensional</span>",
+                   "commission" => "<span class='swap'>big boy party</span>",
+                    "Washington" => "<span class='swap'>Washing-fun-in-the-sun</span>",
+                      " tax" => "<span class='swap'>tithings, m'lord</span>",
+                       "business" => "<span class='swap'>cash magic</span>",
+                        "refused" => "<span class='swap'>scoffed, loud, hard, and deep into the night about</span> ",
+                         " region" => "<span class='swap'>beastlands</span>",
+                          "Europe" => "<span class='swap'>That Nicey-nicey continent</span>",
+                           "British" => "<span class='swap'>Proto-American (aka British)</span>",
+                            "minister" => "<span class='swap'>optimus</span>",
+                             "spending" => "<span class='swap'>money torturing</span>",
+                              "Britain" => "<span class='swap'>America's Grandpappy</span>, Brittania",
+                               "senator" => "bug-lord", "Senate" => "<span class='swap'>Swarm of the bug-lords</span>",
+                                "congress" => "<span class='swap'>bongress</span>",
+                                 "congressional" => "<span class='swap'>bongressional</span>",
+                                  "Pence" => "<span class='swap'>Reed Richards (aka Mr. Fantastic)</span>",
+                                   "argues" => "<span class='swap'>eagle-screeches from the roosts of the city</span>", 
+                                   "government" => "<span class='swap'>fun police</span>",
+                                    "women" => "<span class='swap'>reverse-men, anti-boys, some brave adventurers know them as lady-folk,</span>",
+                                     "story" => "<span class='swap'>a tale that would rend your ass on asunder</span>",
+                                      "recently" => "<span class='swap'>schmecently</span>",
+                                       "public" => "<span class='swap'>errybody's</span>",
+                                        "pollution" => "<span class='swap'>mung of the earth</span>",
+                                         "American" => "<span class='swap'>Boopmerican</span>",
+                                          "american" => "<span class='swap'>schmoopmerican</span>",
+                                           "Mueller" => "<span class='swap'>\"Platinum Johnson\" Mueller</span>", "recused" => "<span class='swap'>\"brocused\"</span>" }
 
     matchers = substitution_hash
     body.gsub(/Trump|Mr. Trump|president|President|presidency|Sessions|US|economic|country|global|commission|Washington|tax|business|refused|region|Europe|British|minister|spending|Britain|senator|Senate|congress|congressional|Pence|argues|government|women|story|recently|public|pollution|American|american|Mueller|recused/){ |match| matchers[match] } 
@@ -14,49 +45,110 @@ class Article < ApplicationRecord
   end
 
   def swap_headline
-    substitution_hash = { "Trump" => "\"Mothertruckin\" Trump",
-                "Mr. Trump" => "Donald \"Mothertruckin\" Trump",
-               "president" => "hyperdimensional samurai",
-               "President" => "Hyperdimensional samurai",
-              "presidency" => "glorious and honorable title of hyperdimensional samurai", "presidency" => "glorious and honorable title of hyperdimensional samurai", "Sessions" => "meatboy", "US" => "The United Snapes of America", "economic" => "money-shenanigans", "country" => "citizen festival site", " global" => "transdimensional", "commission" => "fun-time boy party", "Washington" => "The fertile womb of government", " tax" => "tithings, m'lord", "business" => "cash magic", "refused" => "scoffed, loud and hard about ", " region" => "beastlands", "Europe" => "That Nicey-nicey continent", "British" => "proto-American (aka British, son)", "minister" => "optimus", "spending" => "money torturing", "Britain" => "America's Grandpappy, Brittania", "senator" => "bug-lord", "Senate" => "Swarm of the bug-lords", "congress" => "bongress", "congressional" => "bongressional", "Pence" => "Reed Richards (aka Mr. Fantastic)", "argues" => "eagle-screeches from the roosts of the city", "government" => "fun police", "women" => "non-men, the fairer sex i'faith,", "story" => "a tale that would set your ass on fire", "recently" => "schmecently", "public" => "errybody's", "pollution" => "mung of the earth", "American" => "Boopmerican", "american" => "schmoopmerican", "Mueller" => "\"Platinum Johnson\" Mueller", "recused" => "\"brocused\"" }
+    substitution_hash = { "Trump" => "<span class='swap'>\"Mothertruckin\" Trump</span>",
+                "Mr. Trump" => "<span class='swap'>Mr. Trump</span>",
+               "president" => "<span class='swap'>hyperdimensional samurai</span>",
+               "President" => "<span class='swap'>Hyperdimensional samurai</span>",
+              "presidency" => "<span class='swap'>jellybean days</span>", "Sessions" => "<span class='swap'>meatboy</span>",
+               "US" => "<span class='swap'>The United Snapes of America</span>",
+                "economic" => "<span class='swap'>money-shenanigans</swap>",
+                 "country" => "<span class='swap'>citizen festival site</span>",
+                  " global" => "<span class='swap'>transdimensional</span>",
+                   "commission" => "<span class='swap'>big boy party</span>",
+                    "Washington" => "<span class='swap'>Washing-fun-in-the-sun</span>",
+                      " tax" => "<span class='swap'>tithings, m'lord</span>",
+                       "business" => "<span class='swap'>cash magic</span>",
+                        "refused" => "<span class='swap'>scoffed, loud, hard, and deep into the night about</span> ",
+                         " region" => "<span class='swap'>beastlands</span>",
+                          "Europe" => "<span class='swap'>That Nicey-nicey continent</span>",
+                           "British" => "<span class='swap'>Proto-American (aka British)</span>",
+                            "minister" => "<span class='swap'>optimus</span>",
+                             "spending" => "<span class='swap'>money torturing</span>",
+                              "Britain" => "<span class='swap'>America's Grandpappy</span>, Brittania",
+                               "senator" => "bug-lord", "Senate" => "<span class='swap'>Swarm of the bug-lords</span>",
+                                "congress" => "<span class='swap'>bongress</span>",
+                                 "congressional" => "<span class='swap'>bongressional</span>",
+                                  "Pence" => "<span class='swap'>Reed Richards (aka Mr. Fantastic)</span>",
+                                   "argues" => "<span class='swap'>eagle-screeches from the roosts of the city</span>", 
+                                   "government" => "<span class='swap'>fun police</span>",
+                                    "women" => "<span class='swap'>the fairer sex, yon laydies i'faith,</span>",
+                                     "story" => "<span class='swap'>a tale that would rend your ass on asunder</span>",
+                                      "recently" => "<span class='swap'>schmecently</span>",
+                                       "public" => "<span class='swap'>errybody's</span>",
+                                        "pollution" => "<span class='swap'>mung of the earth</span>",
+                                         "American" => "<span class='swap'>Boopmerican</span>",
+                                          "american" => "<span class='swap'>schmoopmerican</span>",
+                                           "Mueller" => "<span class='swap'>\"Platinum Johnson\" Mueller</span>", "recused" => "<span class='swap'>\"brocused\"</span>",
+                                            'Democrats' => '<div class="animated pulse">Demogorgons</div>'}
 
     matchers = substitution_hash
-    headline.gsub(/Trump|Mr. Trump|president|President|presidency|Sessions|US|economic|country|global|commission|Washington|tax|business|refused|region|Europe|British|minister|spending|Britain|senator|Senate|congress|congressional|Pence|argues|government|women|story|recently|public|pollution|American|american|Mueller|recused/){ |match| matchers[match] }
+    headline.gsub(/Trump|Mr. Trump|president|President|presidency|Sessions|US|economic|country|global|commission|Washington|tax|business|refused|region|Europe|British|minister|spending|Britain|senator|Senate|congress|congressional|Pence|argues|government|women|story|recently|public|pollution|American|american|Mueller|recused|Democrats/){ |match| matchers[match] }
   end
 
   def add_headline
-    addition_hash = { "million" => "million dollars worth of bees, glorious bees",
-                      "Tax" => "Tax baby, tax",
-                      "tax" => "tax, or as ya boy Donny Trump says it: 'terx'",
-                      "taxes" => ", or as ya boy Donny Trump likes to call them: 'torxes'","elections" => "elections - more like ERECTIONS, am I right? Wait, sorry... elections." }
+    addition_hash = { "Press" => "<span class='add'>Prestidigitators, my dear boy!</span>","press" => "<span class='add'>prestidigitators</span>", "Trump" => "<span class='add'>Trumpet (imagine the saxophone lick from 'Careless Whisper' right now, you won't regret it)</span>", "million" => "<span class='add'>million dollars worth of bees, glorious bees</span>",
+                      "Tax" => "<span class='add'>Tax baby, tax</span>",
+                      "tax" => "<span class='add'>tax, or as ya boy Donny Trump says it: 'terx'</span>",
+                      "taxes" => "<span class='add'>, or as ya boy Donny Trump likes to call them: 'torxes'</span>",
+                      "elections" => "<span class='add'>elections - more like ERECTIONS, am I right? Wait, sorry... elections.</span>",
+                       "liberal" => "<span class='add'>liberal application of topical ointment to the hindquarters</span>",
+                        "Liberal" => "<span class='add'>Liberal, by which we mean a fowl of the eastern seaboard,</span>",
+                         "democrat" => "<span class='add'>democrat, the other white meat</span>",
+                          "Democrat" => "<span class='add'>Democrat, which is latin for 'a free version of the software' plus 'crat'</span>",
+                           "imposed" => "<span class='add'>imposed with great vengeance and furious anger these </span>",
+                            "very" => "<span class='add'>very minimal</span>",
+                            "tariffs" => "<span class='add'>tariffs, like from that one movie Star Wars: the Phantom Tarrifs</span>",
+                             "China" => "<span class='add'>China, land of wind and oats</span>",
+                              "conversation" => "<span class='add'>stimulating conversation over martinis</span>",
+                               "argument" => "<span class='add'>argument of electro-sabers as they clash and vie for the supremacy of their mighty weilders</span>",
+                                "houses" => "<span class='add'>haunted houses</span>",
+                                 "democracy" => "<span class='add'>democracy, more like demoCRAZY, am I right friend? No? Ah, I guess you had to be there...    CONTINUING:</span>",
+                                 "quote" => "<span class='add'>quothe</span>" 
+                    }
 
     matchers = addition_hash
-    headline.gsub(/elections|million|Tax|tax|taxes/){ |match| matchers[match] }    
+    headline.gsub(/Press|press|Trump|elections|million|Tax|tax|taxes|liberal|Liberal|democrat|Democrat|imposed|very|tariffs|China|conversation|argument|houses|democracy|quote/){ |match| matchers[match] }    
   end
 
 
   def add_body
-    addition_hash = { "million" => "million dollars worth of bees, glorious bees",
-                      "Tax" => "Tax baby, tax",
-                      "tax" => "tax, or as ya boy Donny Trump says it: 'terx'",
-                      "taxes" => "taxes, or as ya boy Donny Trump likes to call them: 'torxes'",
-                      "elections" => "elections - more like ERECTIONS, am I right? Wait, sorry... elections." }
+    addition_hash = { "Press" => "<span class='add'>Prestidigitators, my dear boy!</span>","press" => "<span class='add'>prestidigitators</span>", "Trump" => "<span class='add'>Trumpet (imagine the big brass chorus of 'Oh when the Saints go marching in')</span>", "million" => "<span class='add'>million dollars worth of bees, glorious bees</span>",
+                      "Tax" => "<span class='add'>Tax baby, tax</span>",
+                      "tax" => "<span class='add'>tax, or as ya boy Donny Trump says it: 'terx'</span>",
+                      "taxes" => "<span class='add'>, or as ya boy Donny Trump likes to call them: 'torxes'</span>",
+                      "elections" => "<span class='add'>elections - more like ERECTIONS, am I right? Wait, sorry... elections.</span>",
+                       "liberal" => "<span class='add'>liberal application of topical ointment to the hindquarters</span>",
+                        "Liberal" => "<span class='add'>Liberal, by which we mean a fowl of the eastern seaboard,</span>",
+                         "democrat" => "<span class='add'>democrat, the other white meat</span>",
+                          "Democrat" => "<span class='add'>Democrowboy, on a steel horse I ride,</span>",
+                           "imposed" => "<span class='add'>imposed with great vengeance and furious anger these </span>",
+                            "very" => "<span class='add'>very minimal</span>",
+                            "tariffs" => "<span class='add'>tariffs, like from that one movie Star Wars: the Phantom Tarrifs</span>",
+                             "China" => "<span class='add'>China, land of wind and oats</span>",
+                              "conversation" => "<span class='add'>stimulating conversation over martinis</span>",
+                               "argument" => "<span class='add'>argument of electro-sabers as they clash and vie for the supremacy of their mighty weilders</span>",
+                                "houses" => "<span class='add'>haunted houses</span>",
+                                 "democracy" => "<span class='add'>democracy, more like demoCRAZY, am I right friend? No? Ah, I guess you had to be there...    CONTINUING:</span>",
+                                 "quote" => "<span class='add'>quothe</span>" 
+                    }
     matchers = addition_hash
-    body.gsub(/elections|million|Tax|tax|taxes/){ |match| matchers[match] }
+    body.gsub(/Press|press|Trump|elections|million|Tax|tax|taxes|liberal|Liberal|democrat|Democrat|imposed|very|tariffs|China|conversation|argument|houses|democracy|quote/){ |match| matchers[match] }    
   end
 
   def redact_headline
     redact_hash = {"Republicans" => "**pub***ns","polls" => "po*es","elections" => "****ions", "t" => " <span class='redacted'>****</span>ing ", "tremendous" => "****ty", "great" => "****ked", "speech" => "**** ****apping", "investigation" => "****k party"}
 
     matchers = redact_hash
-    headline.gsub(/Republicans|polls|elections|t|tremendous|great|speech|investigation/) { |match| matchers[match] }
+    headline.gsub(/Republicans|polls|elections|the|tremendous|great|speech|investigation/) { |match| matchers[match] }
   end
 
   def redact_body
-      redact_hash = {"Republicans" => "**pub***ns","polls" => "po*es","elections" => "****ions", "t" => " <span class='redacted'>****</span>ing ", "tremendous" => "****ty", "great" => "****ked", "speech" => "**** ****apping", "investigation" => "****k party", ' <a href="' => '', ' </a>' => ''}
+      redact_hash = {"Republicans" => "**pub***ns","polls" => "po*es","elections" => "****ions", "the" => " <span class='redacted'>****</span>ing ", "tremendous" => "****ty", "great" => "****ked", "speech" => "**** ****apping", "investigation" => "<span class='redacted'>****</span>k party", "sh" => "sh<span class='redacted'>**</span>", "pick" => "<span class='redacted'>**</span>ck", "Fox" => "F<span class='redacted'>***</span>"}
       
       matchers = redact_hash
-      body.gsub(/Republicans|polls|elections|t|tremendous|great|speech|investigation|<a href="| %[<a>]/) { |match| matchers[match] }
+      body.gsub(/Republicans|polls|elections|the|tremendous|great|speech|investigation|sh|pick|Fox/) { |match| matchers[match] }
   end
 end
+
+# %[<a>]
 
